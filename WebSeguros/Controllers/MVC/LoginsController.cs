3ceeb0c435164823d396ec19adfaa9e16af0214b
Alex Adriano <alexliga1998@outlook.com>
2018-06-26 01:23:37 -0500

    cambios

---
+++ WebSeguros/Controllers/MVC/LoginsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Entidades;
+using Servicios.Interfaces;
+using Entidades.Utils;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace WebSeguros.Controllers.MVC
+{  
+    public class LoginsController : Controller
+    {
+        private readonly IApiServicio apiServicio;
+        public LoginsController(IApiServicio apiServicio)
+        {
+            this.apiServicio = apiServicio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var lista = new List<Login>();
+            try
+            {
+                lista = await apiServicio.Listar<Login>(new Uri(WebApp.BaseAddress)
+                                                                    , "api/Logins/ListaLogin");
+                return View(lista);
+            }
+            catch (Exception ex)
+            {                
+                return BadRequest();
+            }
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewData["IdGenero"] = new SelectList(await apiServicio.Listar<Genero>(new Uri(WebApp.BaseAddress), "api/Generoes/ListarGeneros"), "IdGenero", "Descripcion");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Persona persona)
+        {
+            ViewData["IdGenero"] = new SelectList(await apiServicio.Listar<Genero>(new Uri(WebApp.BaseAddress), "api/Generoes/ListarGeneros"), "IdGenero", "Descripcion");
+            Response response = new Response();
+            try
+            {
+                response = await apiServicio.InsertarAsync(persona,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "api/Personas/InsertarPersona");
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction("Index");
+                }
+                ViewData["IdGenero"] = new SelectList(await apiServicio.Listar<Genero>(new Uri(WebApp.BaseAddress), "api/Generoes/ListarGeneros"), "IdGenero", "Descripcion");
+                return View(persona);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+            
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    var respuesta = await apiServicio.SeleccionarAsync<Response>(id, new Uri(WebApp.BaseAddress),
+                                                                  "api/Personas");
+                    if (respuesta.IsSuccess)
+                    {
+
+                        var respuestaPersona = JsonConvert.DeserializeObject<Persona>(respuesta.Resultado.ToString());
+                        var persona = new Persona
+                        {
+                            IdPersona= respuestaPersona.IdPersona,
+                            Identificacion = respuestaPersona.Identificacion,
+                            Nombre = respuestaPersona.Nombre,
+                            Apellido = respuestaPersona.Apellido
+                        };
+
+                        ViewData["IdGenero"] = new SelectList(await apiServicio.Listar<Genero>(new Uri(WebApp.BaseAddress), "api/Generoes/ListarGeneros"), "IdGenero", "Descripcion");
+                        
+                        return View(persona);
+                    }
+
+                }
+
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, Persona persona)
+        {
+            Response response = new Response();
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    response = await apiServicio.EditarAsync(id, persona, new Uri(WebApp.BaseAddress),
+                                                                 "api/Personas");
+                    if (response.IsSuccess)
+                    {                       
+
+                        return RedirectToAction("Index");
+                    }
+                    ViewData["IdGenero"] = new SelectList(await apiServicio.Listar<Genero>(new Uri(WebApp.BaseAddress), "api/Generoes/ListarGeneros"), "IdGenero", "Descripcion");
+                    return View(persona);
+
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+               
+                return BadRequest();
+            }
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            try
+            {
+                var response = await apiServicio.EliminarAsync(id, new Uri(WebApp.BaseAddress)
+                                                               , "api/Personas");
+                if (response.IsSuccess)
+                {
+                    
+                    return RedirectToAction("Index");
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                
+
+                return BadRequest();
+            }
+        }
+
+        // GET: Personas
+
+    }
+}
